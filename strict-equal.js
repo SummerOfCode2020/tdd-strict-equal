fd82eb0a769a6af1a5e27dda5bf56c47ad74b216
Wesley <wesley.jaboin@gmail.com>
2020-10-01 17:31:20 -0400

    late PR

---
+++ strict-equal.js
@@ -0,0 +1,13 @@
+module.exports = sEqual
+function sEqual(para1, para2){
+    let A = "true"
+    let B = " false"
+if (para1 === para2){
+    console.log(A)
+}
+else {
+    console.log(B)
+}
+
+}
+sEqual( 10, "10");
